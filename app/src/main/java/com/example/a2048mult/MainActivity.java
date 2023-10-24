31bfe8a222e4113e75bdaffea2ae19f763aec378
jusnim <justin.nimz@gmx.de>
2023-10-24 23:56:02 +0200

    Added Title and Buttons for Main Activity

--- app/src/main/java/com/example/a2048mult/MainActivity.java
+++ app/src/main/java/com/example/a2048mult/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a2048mult;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,9 +10,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        TextView welcomeMessageTV = new TextView(this);
-        welcomeMessageTV.setText("Hello CodeYourApp World!");
-        setContentView(R.layout.main_activity_layout);
+
+        // create Function for buttonSingleplayer
+        final Button buttonSinglePlayer = findViewById(R.id.buttonSingleplayer);
+        buttonSinglePlayer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+
+        // create Function for buttonMultiplayer
+        final Button buttonMultiplayer = findViewById(R.id.buttonMultiplayer);
+        buttonMultiplayer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
     }
 }
