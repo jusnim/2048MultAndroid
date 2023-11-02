c9f75874ff0cbc154ea1e59241703d159151b554
jusnim <justin.nimz@gmx.de>
2023-11-02 23:46:23 +0100

    simplified Fragments, now its more understandable

--- app/src/main/java/com/example/a2048mult/MainActivity.java
+++ app/src/main/java/com/example/a2048mult/MainActivity.java
@@ -1,19 +1,18 @@
 package com.example.a2048mult;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+import com.example.a2048mult.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
     }
 }
