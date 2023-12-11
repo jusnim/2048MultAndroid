518d23d36f5ae909219425555daea6d8e061416e
jusnim <justin.nimz@gmx.de>
2023-12-11 23:39:55 +0100

    rename

--- app/src/main/java/com/example/a2048mult/game/logic/ReceiveListener.java
+++ app/src/main/java/com/example/a2048mult/game/logic/ReceiveListener.java
@@ -3,7 +3,7 @@ package com.example.a2048mult.game.logic;
 /**
  * Interface zum bemerken von neuen Paketen in der 2048-Komponente
  */
-public interface ReciveListener {
+public interface ReceiveListener {
     /**
      * Methode zum Bemerkbarmachen von neuen Informationene in der 2048-Komponente
      */
