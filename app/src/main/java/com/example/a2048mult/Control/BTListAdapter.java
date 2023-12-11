f516f620de0f3011014d5e7edeab329c1ffada68
Rlpragst <temp2@pragst.de>
2023-12-11 03:53:33 +0100

    :BTListAdapter.java

---
+++ app/src/main/java/com/example/a2048mult/Control/BTListAdapter.java
@@ -0,0 +1,67 @@
+package com.example.a2048mult.Control;
+
+import android.bluetooth.BluetoothDevice;
+import android.service.controls.Control;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+
+
+
+import java.util.ArrayList;
+
+public class BTListAdapter extends BaseAdapter {
+
+
+    public ArrayList<BluetoothDevice> btDevicesList;
+    public BluetoothDevice clickedDevice;
+
+    public BTListAdapter() {
+        btDevicesList = new ArrayList<BluetoothDevice>();
+        clickedDevice = null;
+    }
+
+    public void add(BluetoothDevice device) {
+        btDevicesList.add(device);
+    }
+
+    public void clear() {
+        btDevicesList.clear();
+    }
+
+    public void updateAdapter() {
+        notifyDataSetChanged();
+    }
+
+    public void setClickedDevice(BluetoothDevice btDevice) {
+        clickedDevice = btDevice;
+    }
+    public BluetoothDevice getClickedBtDevice() {
+        return clickedDevice;
+    }
+
+    @Override
+    public int getCount() {
+        return btDevicesList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return btDevicesList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        /*BluetoothDevice btDevice = btDevicesList.get(position);
+        TextView textView = new TextView(this);
+        textView.setText(btDevice.getName() + ": " + btDevice.getAddress());*/
+        return null;
+    }
+}
