a35c0cf520fd1f4c061a2458c7709911d7276e05
Nick <niklas.paggel@web.de>
2023-12-11 22:09:27 +0100

    ReciveListener erstellt.

---
+++ app/src/main/java/com/example/a2048mult/game/logic/ReciveListener.java
@@ -0,0 +1,11 @@
+package com.example.a2048mult.game.logic;
+
+/**
+ * Interface zum bemerken von neuen Paketen in der 2048-Komponente
+ */
+public interface ReciveListener {
+    /**
+     * Methode zum Bemerkbarmachen von neuen Informationene in der 2048-Komponente
+     */
+    void onRecivedPaket();
+}
