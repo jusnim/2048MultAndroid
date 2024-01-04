9377b8b8038c05c10caf4d02909d0f1f945da52b
jusnim <justin.nimz@gmx.de>
2024-01-04 16:57:05 +0100

    added implementation of nick for gamelogic and adjusted datatypes

--- app/src/main/java/com/example/a2048mult/game/logic/GameRules.java
+++ app/src/main/java/com/example/a2048mult/game/logic/GameRules.java
@@ -1,24 +1,106 @@
 package com.example.a2048mult.game.logic;
 
-/**
- * specification for rules for the game
- */
-public interface GameRules {
+public class GameRules {
+    public static void moveUp(PlayfieldState game){
+        rotateAntiClockwise(game);
+        rotateAntiClockwise(game);
+        rotateAntiClockwise(game);
+        move(game);
+        rotateAntiClockwise(game);
+    }
 
-    /**
-     * applies the rules when moving left
-     */
-    void applyMoveLeft();
-    /**
-     * applies the rules when moving right
-     */
-    void applyMoveRight();
-    /**
-     * applies the rules when moving up
-     */
-    void applyMoveUp();
-    /**
-     * applies the rules when moving down
-     */
-    void applyMoveDown();
+    public static void moveDown(PlayfieldState game){
+        rotateAntiClockwise(game);
+        move(game);
+        rotateAntiClockwise(game);
+        rotateAntiClockwise(game);
+        rotateAntiClockwise(game);
+    }
+    public static void moveLeft(PlayfieldState game){
+        move(game);
+    }
+    public static void moveRight(PlayfieldState game){
+        rotateAntiClockwise(game);
+        rotateAntiClockwise(game);
+        move(game);
+        rotateAntiClockwise(game);
+        rotateAntiClockwise(game);
+    }
+
+    public static boolean spawnTile(PlayfieldState game){
+        //TODO: random
+        double[][] random = new double[game.getFieldSizeX()][game.getFieldSizeY()];
+        int[] highest = new int[2];
+        double highestValue = 0;
+        for(int i = 0; i < game.getFieldSizeX(); i++){
+            for(int j = 0; j < game.getFieldSizeY(); j++){
+                random[i][j] = Math.random();
+                if(random[i][j] > highestValue){
+                    highestValue = random[i][j];
+                    highest[0] = i;
+                    highest[1] = j;
+                }
+            }
+        }
+
+        while(game.getTile(highest[0],highest[1]) != 0){
+            random[highest[0]][highest[1]] = 0;
+            highestValue = 0;
+            for(int i = 0; i < game.getFieldSizeX(); i++){
+                for(int j = 0; j < game.getFieldSizeY(); j++){
+                    if(random[i][j] > highestValue){
+                        highestValue = random[i][j];
+                        highest[0] = i;
+                        highest[1] = j;
+                    }
+                }
+            }
+        }
+        if(highestValue == 0)
+            return false; //spiel verloren
+        int tileValue = 1;
+        if(Math.random() > 0.8)
+            tileValue = 2;
+        game.setTile(highest[0], highest[1], tileValue);
+        return true;
+    }
+
+    public static void move(PlayfieldState game){    //move links als standard
+        int[][] neueListe = new int[game.getFieldSizeX()][game.getFieldSizeY()];
+
+        //spielfeld nach move erstellen
+        for(int x = 0; x <game.getFieldSizeX(); x++){
+            int positionNeueListe = 0;
+            int zahl = game.getTile(x, 0);
+            for(int y = 1; y< game.getFieldSizeY(); y++){
+                if(zahl == 0){
+                    zahl = game.getTile(x, y);
+                }else if(zahl == game.getTile(x, y)){
+                    neueListe[x][positionNeueListe] = zahl+1;
+                    zahl = 0;
+                    positionNeueListe++;
+                }else if(game.getTile(x,y) != 0){
+                    neueListe[x][positionNeueListe] = zahl;
+                    zahl = game.getTile(x,y);
+                    positionNeueListe++;
+                }
+            }
+            neueListe[x][positionNeueListe] = zahl;
+        }
+
+        //spielfeld einfügen
+        game.setField(neueListe);
+    }
+
+    public static void rotateAntiClockwise(PlayfieldState game){
+        int[][] field = game.getField();
+        int[][] output = new int[game.getFieldSizeX()][game.getFieldSizeY()];
+        for(int y = 0; y < game.getFieldSizeY(); y++){
+            for (int x = 0; x < game.getFieldSizeX(); x++){
+                output[x][y] = field[game.getFieldSizeY()-y-1][x];
+            }
+        }
+        game.setField(output);
+    }
+
 }
---
+++ app/src/main/java/com/example/a2048mult/game/logic/PlayfieldStateImpl.java
@@ -0,0 +1,73 @@
+package com.example.a2048mult.game.logic;
+
+public class PlayfieldStateImpl implements PlayfieldState{
+    /**
+     * The GameState class which provides the playing field.
+     * @author Niklas Paggel
+     */
+    /**
+     * field[x][y] startet bei 0, 0
+     */
+    private final int fieldSizeX;
+    private final int fieldSizeY;
+
+    private final int field[/*X*/][/*Y*/];  //0, 0 ist oben links
+    public PlayfieldStateImpl(int fieldSizeX, int fieldSizeY){
+        if(fieldSizeX < 1 || fieldSizeY < 1){
+            throw new IllegalArgumentException("You need at least 1 in size to have a playing field at all");
+        }
+        this.fieldSizeX = fieldSizeX;
+        this.fieldSizeY = fieldSizeY;
+        field = new int[fieldSizeX][fieldSizeY];
+    }
+
+    public PlayfieldStateImpl(int fieldSize){
+        this(fieldSize,fieldSize);
+    }
+    public PlayfieldStateImpl(){
+        this(4);
+    }
+
+    /**
+     * für Multiplayer um Tiles zu setzen
+     * @param x Coordinate
+     * @param y Coordinate
+     * @param value of tile
+     * @return
+     */
+    @Override
+    public boolean setTile(int x, int y, int value){
+        //TODO: frei Felder besetzen
+        if(field[x][y] != 0){
+            return false;
+        }
+        field[x][y] = value;
+        return true;
+    }
+
+    public int getFieldSizeX(){
+        return fieldSizeX;
+    }
+
+    public int getFieldSizeY(){
+        return fieldSizeY;
+    }
+
+    @Override
+    public int getTile(int x, int y){
+        return field[x][y];
+    }
+
+    @Override
+    public int[][] getField(){
+        return field;
+    }
+
+
+    @Override
+    public void setField(int[][] field){
+        for(int x = 0; x<getFieldSizeX(); x++){
+            if (getFieldSizeY() >= 0) System.arraycopy(field[x], 0, this.field[x], 0, getFieldSizeY());
+        }
+    }
+}
